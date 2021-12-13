0dc5c89d4d67e7e5c6bd78e142cde557979d6094
Dylan-Bouquet <dylanbouquet66@gmail.com>
2021-12-13 19:34:54 +0100

    Partie 'frontend' et partie 'backend' api secure

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const stuffRoutes = require('./routes/stuff');
+
+mongoose.connect('mongosh "mongodb+srv://api-secure-cluster.2w8uz.mongodb.net/Api-Secure-Cluster"',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/api/stuff', stuffRoutes);
+
+module.exports = app;
